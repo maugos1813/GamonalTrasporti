93a4a5a0b0c53ff31dcffb839bbb58dab2146b8f
maugos1302 <maufabagosgam@gmail.com>
2024-11-26 17:57:51 -0500

    correccion de colores en nav, cambio de logo, imagenes de servicios provicional

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import menuIcon from '/menu.png';
-import logom from '/logom.png';
+import logom from '/logogam.jpeg';
 
 export const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,27 +15,26 @@ export const Nav = () => {
   };
 
   return (
-    <nav className="bg-white shadow-lg border-black h-[10vh] relative z-50 rounded-xl">
+    <nav className="bg-white shadow-lg border h-[10vh] relative z-50 rounded-b-xl border-green-500">
       <div className="max-w-7xl mx-auto px-4 h-full">
         <div className="flex justify-between items-center h-full">
           {/* Logo */}
           <div className="flex ml-5 items-center">
             <img src={logom} alt="medicine icon" className="h-[8vh]" />
-            <h1 className="text-[3vh] font-sans">ClanGamonal</h1>
           </div>
 
           {/* Menú grande */}
           <div className="hidden sm:flex flex-grow justify-center items-center space-x-4">
-            <Link to="/" className="text-gray-700 hover:text-gray-900 text-[2vh] hover:bg-sky-300 w-[10vh] flex justify-center rounded-md">Home</Link>
-            <Link to="/services" className="text-gray-700 hover:text-gray-900 text-[2vh] hover:bg-sky-300 w-[10vh] flex justify-center rounded-md">Services</Link>
-            <Link to="/about" className="text-gray-700 hover:text-gray-900 text-[2vh] hover:bg-sky-300 w-[10vh] flex justify-center rounded-md">About</Link>
-            <Link to="/contact" className="text-gray-700 hover:text-gray-900 text-[2vh] hover:bg-sky-300 w-[10vh] flex justify-center rounded-md">Contact</Link>
+            <Link to="/" className="text-gray-700 hover:text-gray-900 text-[2vh] hover:bg-green-300 w-[10vh] flex justify-center rounded-md">Home</Link>
+            <Link to="/about" className="text-gray-700 hover:text-gray-900 text-[2vh] hover:bg-green-300 w-[10vh] flex justify-center rounded-md">Chi Siamo</Link>
+            <Link to="/services" className="text-gray-700 hover:text-gray-900 text-[2vh] hover:bg-green-300 w-[10vh] flex justify-center rounded-md">Servizi</Link>
+            <Link to="/contact" className="text-gray-700 hover:text-gray-900 text-[2vh] hover:bg-green-300 w-[10vh] flex justify-center rounded-md">Contatti</Link>
           </div>
 
           {/* Botón "Regístrate ahora" siempre al final */}
           <div className="hidden sm:flex justify-end">
-            <button className="ml-4 px-6 py-2 bg-blue-500 text-white text-[2vh] rounded-lg hover:bg-blue-600 h-[6vh]">
-              Contáctanos ahora
+            <button className="ml-4 px-6 py-2 bg-green-400 text-white text-[2vh] rounded-lg hover:bg-green-300 h-[6vh]">
+            Contattaci ora
             </button>
           </div>
 
@@ -52,23 +51,23 @@ export const Nav = () => {
           <div className="sm:hidden bg-slate-600 w-full z-50">
             <ul className="flex flex-col mt-1 space-y-2 bg-white w-full">
               <li className='shadow-lg rounded-xl'>
-                <Link to="/" onClick={handleLinkClick} className="block px-4 py-2 text-gray-700 hover:text-gray-900">
+                <Link to="/" onClick={handleLinkClick} className="block px-4 py-2 text-gray-700 hover:text-gray-900 border-green-300 ">
                   Home
                 </Link>
               </li>
               <li className='shadow-lg rounded-xl'>
                 <Link to="/services" onClick={handleLinkClick} className="block px-4 py-2 text-gray-700 hover:text-gray-900">
-                  Services
+                  Servizi
                 </Link>
               </li>
               <li className='shadow-lg rounded-xl'>
                 <Link to="/about" onClick={handleLinkClick} className="block px-4 py-2 text-gray-700 hover:text-gray-900">
-                  About
+                  Chi Siamo
                 </Link>
               </li>
               <li className='shadow-lg rounded-xl' >
                 <Link to="/contact" onClick={handleLinkClick} className="block px-4 py-2 text-gray-700 hover:text-gray-900">
-                  Contact
+                  Contatti
                 </Link>
               </li>
             </ul>
