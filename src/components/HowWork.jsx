939b9dd783db9565e4be250ab0a73f066cadde4a
Maugos1302 <maufabagosgam@gmail.com>
2024-12-12 21:52:22 -0500

    cambios en HowWork

--- src/components/HowWork.jsx
+++ src/components/HowWork.jsx
@@ -1,12 +1,68 @@
-import React from 'react'
+import React from "react";
+import entregando from "/entregando.webp";
+import temp from "/temperatgris.png";
 
 export const HowWork = () => {
   return (
-    <div className='bg-gray-100'>
-        <div className='flex flex-col items-center justify-center'>
-            <p className='mt-[100px] border-l-4 border-green-500 w-[140px] bg-gray-200 text-center'>Our Goodness</p>
-            <h1 className='mt-[20px] text-[35px] mb-[100px]'>How we works</h1>
+    <div className="bg-gray-100">
+      <div className="flex flex-col items-center justify-center">
+        <p className="mt-[100px] border-l-4 border-green-500 w-[140px] bg-gray-200 text-center">
+          Our Goodness
+        </p>
+        <h1 className="mt-[20px] text-[4vh] mb-[80px]">How we works</h1>
+      </div>
+
+      <div className="lg:flex lg:gap-10 lg:pb-[100px]">
+        <div className="flex items-center justify-center lg:items-start lg:justify-start">
+          <img
+            src={entregando}
+            alt="giving a packet image"
+            className="w-[90vw] rounded-3xl md:w-[50vw] lg:w-[45vw] lg:ml-10"
+          />
         </div>
+
+        <div className="flex flex-col items-center justify-center mt-[80px] pb-[80px] gap-16 lg:mt-[0px] lg:justify-center lg:items-center">
+          <div className="flex w-[80vw] gap-8 lg:w-[35vw]">
+            <div>
+              <img
+                src={temp}
+                alt="temperature icon"
+                className="w-[15vw] lg:w-[10vw]"
+              />
+            </div>
+            <div className="">
+              <h3 className="text-[3vh]">Prueba de Títutlo</h3>
+              <p>texrto de prueba para mejorar en programación</p>
+            </div>
+          </div>
+          <div className="flex w-[80vw] gap-8 lg:w-[35vw]">
+            <div>
+              <img
+                src={temp}
+                alt="temperature icon"
+                className="w-[15vw] lg:w-[10vw]"
+              />
+            </div>
+            <div className="">
+              <h3 className="text-[3vh]">Prueba de Títutlo</h3>
+              <p>texrto de prueba para mejorar en programación</p>
+            </div>
+          </div>
+          <div className="flex w-[80vw] gap-8 lg:w-[35vw]">
+            <div>
+              <img
+                src={temp}
+                alt="temperature icon"
+                className="w-[15vw] lg:w-[10vw]"
+              />
+            </div>
+            <div className="">
+              <h3 className="text-[3vh]">Prueba de Títutlo</h3>
+              <p>texrto de prueba para mejorar en programación</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
