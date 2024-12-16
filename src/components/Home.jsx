c25ecf4f8bc1d4883d526353682563d598194d68
Maugos1302 <maufabagosgam@gmail.com>
2024-12-16 18:54:28 -0500

    botones contacto a contacto

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,7 @@
-import logogam from "/logogam.jpeg";
-import time from "/time.png";
-import vehi from "/vehiculo.png";
-import temp from "/temperatgris.png";
 import { TypeServices } from "./TypeServices.jsx";
 import { HowWork } from "./HowWork.jsx";
 import { Contact } from "./Contact.jsx";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
@@ -12,26 +9,28 @@ export const Home = () => {
       <div className="flex flex-col bg-cover bg-center bg-[url('/camioni.jpg')] h-[80vh]">
         <div className="mb-4">
           <h1 className="text-[5vh] text-center px-3 mt-[80px] ml-5 text-white font-bold lg:text-start lg:mt-24">
-          Il nostro servizio logistico!
+            Il nostro servizio logistico!
           </h1>
         </div>
 
         <div className="flex flex-col md:flex-row">
           <div className="md:w-full p-3">
             <div className="flex items-center justify-center lg:justify-start">
-              <button className="ml-4 mt-4 px-6 py-2 bg-[#252d5c] text-white text-[2vh] sm:text-[2.5vh] rounded-lg hover:bg-green-500 h-[8vh] ">
-                Contattaci ora
-              </button>
+              <Link to="/contact">
+                <button className="ml-4 mt-4 px-6 py-2 bg-[#252d5c] text-white text-[2vh] sm:text-[2.5vh] rounded-lg hover:bg-green-500 h-[8vh] ">
+                  Contattaci ora
+                </button>
+              </Link>
             </div>
           </div>
         </div>
       </div>
 
-      <TypeServices/>
+      <TypeServices />
 
-      <HowWork/>
+      <HowWork />
 
-      <Contact/>
+      <Contact />
     </>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import menuIcon from '/more.png';
-import logom from '/logoc.jpeg';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import menuIcon from "/more.png";
+import logom from "/logoc.jpeg";
 
 export const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,23 +19,47 @@ export const Nav = () => {
       <div className="max-w-7xl mx-auto px-4 h-full">
         <div className="flex justify-between items-center h-full">
           <div className="flex items-center ">
-            <img src={logom} alt="medicine icon" className="h-[8vh] rounded-full p-2" />
+            <img
+              src={logom}
+              alt="medicine icon"
+              className="h-[8vh] rounded-full p-2"
+            />
           </div>
 
           <div className="hidden lg:flex flex-grow justify-center items-center space-x-4">
-            <Link to="/" className="text-white hover:text-gray-900 text-[2vh] hover:bg-green-500 w-[10vh] flex justify-center rounded-md">Home</Link>
-            <Link to="/services" className="text-white hover:text-gray-900 text-[2vh] hover:bg-green-500 w-[10vh] flex justify-center rounded-md">Servizi</Link>
-            <Link to="/contact" className="text-white hover:text-gray-900 text-[2vh] hover:bg-green-500 w-[10vh] flex justify-center rounded-md">Contatti</Link>
+            <Link
+              to="/"
+              className="text-white hover:text-gray-900 text-[2vh] hover:bg-green-500 w-[10vh] flex justify-center rounded-md"
+            >
+              Home
+            </Link>
+            <Link
+              to="/services"
+              className="text-white hover:text-gray-900 text-[2vh] hover:bg-green-500 w-[10vh] flex justify-center rounded-md"
+            >
+              Servizi
+            </Link>
+            <Link
+              to="/contact"
+              className="text-white hover:text-gray-900 text-[2vh] hover:bg-green-500 w-[10vh] flex justify-center rounded-md"
+            >
+              Contatti
+            </Link>
           </div>
 
           <div className="hidden lg:flex justify-end">
-            <button className="ml-4 px-6 py-2 bg-green-500 text-white text-[2vh] rounded-lg hover:bg-green-400 h-[6vh]">
-              Contattaci ora
-            </button>
+            <Link to="/contact">
+              <button className="ml-4 px-6 py-2 bg-green-500 text-white text-[2vh] rounded-lg hover:bg-green-400 h-[6vh]">
+                Contattaci ora
+              </button>
+            </Link>
           </div>
 
           <div className="lg:hidden">
-            <button onClick={toggleMenu} className="text-gray-500 hover:text-gray-700 focus:outline-none">
+            <button
+              onClick={toggleMenu}
+              className="text-gray-500 hover:text-gray-700 focus:outline-none"
+            >
               <img src={menuIcon} alt="Menu icon" className="w-6 h-6" />
             </button>
           </div>
@@ -44,18 +68,30 @@ export const Nav = () => {
         {isOpen && (
           <div className="lg:hidden bg-slate-600 w-full z-50">
             <ul className="flex flex-col bg-[#252d5c] text-white w-full">
-              <li className='shadow-lg rounded-xl'>
-                <Link to="/" onClick={handleLinkClick} className="block px-4 py-2 text-white hover:text-gray-900 border-green-300 ">
+              <li className="shadow-lg rounded-xl">
+                <Link
+                  to="/"
+                  onClick={handleLinkClick}
+                  className="block px-4 py-2 text-white hover:text-gray-900 border-green-300 "
+                >
                   Home
                 </Link>
               </li>
-              <li className='shadow-lg rounded-xl'>
-                <Link to="/services" onClick={handleLinkClick} className="block px-4 py-2 text-white hover:text-gray-900">
+              <li className="shadow-lg rounded-xl">
+                <Link
+                  to="/services"
+                  onClick={handleLinkClick}
+                  className="block px-4 py-2 text-white hover:text-gray-900"
+                >
                   Servizi
                 </Link>
               </li>
-              <li className='shadow-lg rounded-xl'>
-                <Link to="/contact" onClick={handleLinkClick} className="block px-4 py-2 text-white hover:text-gray-900">
+              <li className="shadow-lg rounded-xl">
+                <Link
+                  to="/contact"
+                  onClick={handleLinkClick}
+                  className="block px-4 py-2 text-white hover:text-gray-900"
+                >
                   Contatti
                 </Link>
               </li>
